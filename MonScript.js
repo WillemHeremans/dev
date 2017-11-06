a29bef2ea1f4ef661d92011fff5a08bf8d80c7c9
willemHeremans <willemheremans@gmail.com>
2017-11-06 16:38:39 +0100

    dev

---
+++ MonScript.js
@@ -0,0 +1,14 @@
+var myHeading = document.querySelector('p');
+myHeading.textContent = 'Hello world!';
+
+var boubou = 3;
+if (2 < 5) {
+    boubou = 4;
+}
+
+document.querySelector('html').onclick = function() {
+    alert(boubou);
+}
+
+
+
