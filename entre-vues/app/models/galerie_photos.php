b4b99b38af64e6f114934308443ee37b991d68f0
willemHeremans <willemheremans@gmail.com>
2017-11-09 16:36:26 +0100

    dev

--- entre-vues/app/models/galerie_photos.php
+++ entre-vues/app/models/galerie_photos.php
@@ -36,14 +36,63 @@
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+var img = document.getElementsByTagName("img")[1];
+var modalImg = document.getElementsByClassName("modal-content")[0];
+var captionText = document.getElementsByTagName("img")[1];
 img.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
 }
+
+var img = document.getElementsByTagName("img")[3];
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementsByTagName("img")[3];
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var img = document.getElementsByTagName("img")[5];
+var modalImgFive = document.getElementById("img01");
+var captionText = document.getElementsByTagName("img")[5];
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+		modalImgFive.height = "800";
+}
+
+
+var img = document.getElementsByTagName("img")[7];
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementsByTagName("img")[7];
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var img = document.getElementsByTagName("img")[9];
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementsByTagName("img")[9];
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var img = document.getElementsByTagName("img")[11];
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementsByTagName("img")[11];
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+
 
 
 // Get the <span> element that closes the modal
